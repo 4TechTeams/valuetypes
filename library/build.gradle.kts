90f3d2f5e9c710f4a958c50cfb5e89dbdcdeaafb
frne <fneff89@gmail.com>
2024-11-11 10:51:08 +0100

    Build

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -3,80 +3,33 @@ import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.vanniktech.mavenPublish)
+  alias(libs.plugins.kotlinMultiplatform)
+  alias(libs.plugins.vanniktech.mavenPublish)
+  alias(libs.plugins.kotest.multiplatform)
 }
 
 group = "io.github.kotlin"
 version = "1.0.0"
 
 kotlin {
-    jvm()
-    androidTarget {
-        publishLibraryVariants("release")
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_1_8)
-        }
-    }
-    iosX64()
-    iosArm64()
-    iosSimulatorArm64()
-    linuxX64()
+  jvm()
+  linuxX64()
 
-    sourceSets {
-        val commonMain by getting {
-            dependencies {
-                //put your multiplatform dependencies here
-            }
-        }
-        val commonTest by getting {
-            dependencies {
-                implementation(libs.kotlin.test)
-            }
-        }
+  sourceSets {
+    val commonMain by getting {
+      dependencies {
+        //put your multiplatform dependencies here
+      }
     }
+    val commonTest by getting {
+      dependencies {
+        implementation(libs.kotlin.test)
+        implementation(libs.kotest.framework.engine)
+      }
+    }
+  }
 }
 
-android {
-    namespace = "org.jetbrains.kotlinx.multiplatform.library.template"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-    defaultConfig {
-        minSdk = libs.versions.android.minSdk.get().toInt()
-    }
-}
-
-mavenPublishing {
-    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
-
-    signAllPublications()
-
-    coordinates(group.toString(), "library", version.toString())
-
-    pom {
-        name = "My library"
-        description = "A library."
-        inceptionYear = "2024"
-        url = "https://github.com/kotlin/multiplatform-library-template/"
-        licenses {
-            license {
-                name = "XXX"
-                url = "YYY"
-                distribution = "ZZZ"
-            }
-        }
-        developers {
-            developer {
-                id = "XXX"
-                name = "YYY"
-                url = "ZZZ"
-            }
-        }
-        scm {
-            url = "XXX"
-            connection = "YYY"
-            developerConnection = "ZZZ"
-        }
-    }
+tasks.withType<Test>().configureEach {
+  useJUnitPlatform()
 }
